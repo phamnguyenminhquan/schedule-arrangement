9a136574a7d4a37c3f02354768daa9bedb7e70a5
phamnguyenminhquan <quan.phamnguyen.work@gmail.com>
2026-06-12 22:00:47 +0700

    delete old registration services

--- src/main/services/IRegistrationService.java
+++
@@ -1,8 +0,0 @@
-package main.services;
-
-import main.models.Student;
-import main.models.Subject;
-
-public interface IRegistrationService {
-    void registerSubject(Student student, Subject subject, String sectionId);
-}
